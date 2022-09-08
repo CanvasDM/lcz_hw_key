ec2c7171805ca10d2e41efae4892ff275a7c1391
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-09-08 15:18:36 -0500

    Update test to run
    API changed, update the test.
    
    Update test to run properly on bl5340 dvk, bt610, and mg100.
    
    bug 22568

--- tests/src/main.c
+++ tests/src/main.c
@@ -12,6 +12,7 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #include "lcz_hw_key.h"
 
 #define PLAIN_TEXT_MAX_SIZE (100)
+#define AUTH_DATA "My AUTH data"
 
 static uint8_t plain_text[PLAIN_TEXT_MAX_SIZE] = {
 	"This is an example string that will be encrypted and decrypted."
@@ -25,19 +26,18 @@ void main(void)
 	uint32_t decrypted_data_size;
 	uint8_t iv[LCZ_HW_KEY_IV_LEN];
 
-	lcz_hw_key_generate_and_init();
-
 	lcz_hw_key_generate_iv(iv, LCZ_HW_KEY_IV_LEN);
 
-	lcz_hw_key_encrypt_data((const uint8_t *)iv, LCZ_HW_KEY_IV_LEN, (const uint8_t *)plain_text,
-				strlen(plain_text), encrypted_text, sizeof(encrypted_text),
-				&encrypted_data_size);
+	lcz_hw_key_encrypt_data((const uint8_t *)iv, LCZ_HW_KEY_IV_LEN, AUTH_DATA,
+				sizeof(AUTH_DATA), (const uint8_t *)plain_text, strlen(plain_text),
+				encrypted_text, sizeof(encrypted_text), &encrypted_data_size);
 
 	LOG_HEXDUMP_INF(encrypted_text, encrypted_data_size, "Encrypted data:");
 
-	lcz_hw_key_decrypt_data((const uint8_t *)iv, LCZ_HW_KEY_IV_LEN,
-				(const uint8_t *)encrypted_text, encrypted_data_size,
-				decrypted_text, sizeof(decrypted_text), &decrypted_data_size);
+	lcz_hw_key_decrypt_data((const uint8_t *)iv, LCZ_HW_KEY_IV_LEN, AUTH_DATA,
+				sizeof(AUTH_DATA), (const uint8_t *)encrypted_text,
+				encrypted_data_size, decrypted_text, sizeof(decrypted_text),
+				&decrypted_data_size);
 
 	LOG_INF("Decrypted data: %s", decrypted_text);
 }
